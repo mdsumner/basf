e533f44def87d1124f31a4e16e6f51d30326e264
Michael Sumner <mdsumner@gmail.com>
2020-04-12 14:26:44 +1000

    take out crs stuff

--- R/plot.R
+++ R/plot.R
@@ -1,14 +1,14 @@
 #' Plot simple features simply
 #'
-#' Overrides 'sf::plot.sf' and wraps the call to 'plot(st_geometry(x))'. When
+#' Overrides `sf::plot.sf` and wraps the call to `plot(st_geometry(x))`. When
 #' working with spatial data it's often useful to create maps where we
 #' overlay spatially coincident data in a plot.
 #'
-#' This can be used when developing code or checking things, so that plots
-#' aren't always faceted by all attributes, coloured by them, or leaving
-#' the plot unusable for subsequent additional drawing. The key feature is that
-#' we don't have to change our behaviour and good habits depending on the
-#' format in use.
+#' This is used so that plots aren't always facetted by all attributes, coloured
+#' by them, or leaving the plot unusable for subsequent additional drawing. The
+#' key feature is that we don't have to change our behaviour and good habits
+#' depending on the format in use.
+#'
 #' @param x sf object (the data.frame one)
 #' @param ... arguments passed to [sf::plot_sfc]
 #'
@@ -16,20 +16,16 @@
 #' @export
 #'
 #' @examples
-#' nc <- read_sf(system.file("shape/nc.shp", package="sf"))
+#' x <- read_sf(system.file("shape/nc.shp", package="sf"))
 #' ## all we've changed is the plot command
-#' plot(nc)
+#' plot(x)
 #' ## we can overplot without format-specific acrobatics
-#' plot(nc[sample(1:nrow(nc), 10), ], col = rainbow(10), add = TRUE)
+#' plot(x[sample(1:nrow(x), 10), ], col = rainbow(10), add = TRUE)
 #' abline(v = 34); axis(2)
 #' @importFrom graphics plot
 #' @importFrom sf st_geometry
 plot.sf <- function(x, ...) {
-  ## avoid obstructive errors for non-sf created objects
-  attr(x[[attr(x, "sf_column")]], "crs") <-
-    structure(list(input = NA_character_, wkt = NA_character_), class = "crs")
-
-  plot(sf::st_geometry(x), ...)
+ plot(sf::st_geometry(x), ...)
 }
 
 # just for read_sf
